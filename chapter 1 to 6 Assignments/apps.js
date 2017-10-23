8e1dfdf17a3983bf46a5684df44bcd1d53b4a35d
Asad karim <asadkarim33@gmail.com>
2017-10-23 12:36:45 +0500

    js assignment 1 to 4 completed

---
+++ chapter 1 to 6 Assignments/apps.js
@@ -0,0 +1,119 @@
+//first chapter
+       // part 1
+// alert('Hello World');
+
+       //part 2
+
+// alert('Error! please enter a valid passsord');
+ 
+    //part3
+
+
+// alert('Welcome to js land...\nhappy coding' );
+     
+    // part4
+         
+// alert('Welcome to js land');
+
+        //part 5
+
+
+//alert('Hello i can runjs through my web browers console');
+// console.log('Hello i can runjs through my web browers console');
+
+                        
+                        
+                        // second Chapter
+
+        //part1
+
+// var Username ='';
+
+        //part2
+
+// var myName ="Asad karim Mughal"
+
+        //part3
+
+//message = "hello world";
+//alert(message);
+       
+       //part4
+       
+//   var name = "asad";
+//   var age ="22";
+//   var Education = "Certified Mobile Application Development";
+
+//   alert(name);
+//   alert(age);
+//   alert(Education);
+  
+       // part 5
+
+// alert('pizza \n pizz \n piz \n pi \n p'); 
+
+        // part 6
+
+//var email = "asadkarim33@gmail.com"
+
+//alert('my email address is ' + email);
+
+        //part7
+
+// var mybook = "A smarter way to learn javascript";
+// alert("i am trying to learn from the book " + mybook);
+
+        //part8
+     
+       // document.write('yah! i can write Html Content through javascript');
+
+     //  part9
+
+    //  var sign = "===================";
+    //  alert(sign);
+
+
+        //   chapter 3
+          //  part 1
+
+
+    // var age  = 22;
+    // alert("i am" + age +"year old");
+          
+          //  part 2 incomplete
+
+
+    // var age  = 22;
+    // alert("i am" + age +"year old");
+         //   part3
+
+
+     // var birthyear  = 1996;
+    // document.write('my birth year is '+ birthyear )
+    // document.write('Data type of my variable declaration is number');
+            
+            
+            
+//            part4
+
+
+//     var visitorName = "Asad";
+     //var product = "laptop";
+     
+   //  document.write(visitorName +"has order 2 " +product +" On Karim's Store ");
+
+
+
+//   chapter 4
+
+
+
+  //  part 1
+
+ // 5 illegal variable  5 legal variable 
+
+ //var  +asad           var asad             
+ //var  9               var &asad
+ //var  0asadkarim      var asad_karim          
+ //var  asad-karim      var asadKarim
+//var  alert            userAlert 
