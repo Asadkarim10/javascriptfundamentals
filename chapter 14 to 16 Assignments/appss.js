58b6e309d64b55fff9cb18b6192ea1a99a34f7e4
Asad karim <asadkarim33@gmail.com>
2017-10-27 16:17:02 +0500

    few parts of chapter 15 completes

--- chapter 14 to 16 Assignments/appss.js
+++ chapter 14 to 16 Assignments/appss.js
@@ -14,3 +14,129 @@
 
 //var StudentNames = [1, "asad", "Karim", true];];
     
+    // part6
+//var StudentNames = [true,false];
+
+
+    //part 7
+
+//var edu_Pak = ["Qualifications", "<br>  1) SSC", " <br> 2) HSC", "<br> 3),BCS", "<br> 4)BS", "<br> 5)BCOM", "<br> 6) MS", "<br> 7) M.Phil", "<br> 8)PhD" ];
+//document.write(edu_Pak);
+
+  // part8
+
+//   var studentNames = ["Asad", "Karim", "Mughal"];
+//   var studentScore = [320, 230, 480];
+//   var totalScore = "500";
+
+
+// document.write("Score of " +studentNames[0] +" is "+studentScore[0]+" Percentaga is " + studentScore[0]*100/500, " <br> Score of "+studentNames[1] +" is "+studentScore[1]+" Percentaga is " + studentScore[1]*100/500, " <br/> Score of "+studentNames[2] +" is "+studentScore[2]+" Percentaga is " + studentScore[2]*100/500 );
+
+// part 9
+
+//var color = ["red","blue","green"];
+
+//var color2 = prompt("enter your color name", );
+
+//color.unshift(color2);
+
+//document.write(color);
+
+
+// part 10
+
+
+// var color = ["red","blue","green"];
+
+// var color2 = prompt("enter your color name", );
+
+// color.push(color2);
+
+// document.write(color);
+
+ //part 11
+
+//  var color = ["red","blue","green"]
+
+// color.splice(0, 0, "babyblue" ," pink"); // splice first parameter is asking from where you want to add // and 2nd pare is asking from where you want to remove;
+
+// document.write(color);
+
+//part 12
+
+// var color = ["red ","green","yellow","blue"];
+// color.shift();
+// document.write(color);
+
+//part 13
+
+// var color = ["red ","green","yellow","blue"];
+// color.pop();
+// document.write(color);
+
+//part 14
+
+//  var color = ["red ","green","yellow","blue"];
+
+// var index = prompt("your index");
+// var ind = index;
+
+//  var usr = prompt("your color");
+//  color.splice(ind, 0, usr);
+ 
+// document.write(color);
+
+//part 15 
+
+
+//  var color = ["red ","green","yellow","blue"];
+
+// var index = prompt("your index");
+// var ind = index;
+
+//  //var usr = prompt("your color");
+//  color.splice(ind, ind, );
+ 
+// document.write(color);
+
+
+
+// part 16 sorting array
+
+// var student = ["bilal","asad","cute","ahmed",,"bilawal",];
+// document.write(student + "<br/>" );
+// student.sort();
+// document.write(student);
+
+//part 17
+
+// var cities = ["karachi","lahore","islamabad","quetta","peshawar"];
+// var selectedCities = cities.slice(1,4);
+// document.write(selectedCities);
+
+//part 18
+
+// var arr = ["This", "is", "my","cat"];
+
+// document.write(arr+"<br>");
+// const val = arr.join(' - ');
+
+// document.write(val);
+
+
+// part 19
+
+
+// var newArray = ["keyboard","mouse,printer","monitor"];
+
+// let newArray1 = newArray.sort()
+
+// document.write(newArray1);
+
+
+//var items = ["apple","sony","motorola","iphone","samsung"];
+
+//var items1 =["<select><option value=items[0]>"+items[0]+"</option> <option value=items[1] >"+items[1]+"</option> <option value=items[2] >"+items[2]+"</option> <option>"+items[3]+"</option> <option>"+items[4]+"</option> </select>"]
+
+
+//document.write(items1);
