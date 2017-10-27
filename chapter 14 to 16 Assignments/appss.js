02f39a5d97413ec2b7bd89b261ec1c935054e37d
Asad karim <asadkarim33@gmail.com>
2017-10-27 16:12:25 +0500

    5 parts are completes of chapter 15

---
+++ chapter 14 to 16 Assignments/appss.js
@@ -0,0 +1,16 @@
+// chapter 15
+               
+   //part2
+
+//var studentNames = [];
+
+   //part3
+
+//var StudentNames = ["asad","karim","mughal"];
+    // part4
+
+//var StudentNames = [10,9,8];
+    //part5
+
+//var StudentNames = [1, "asad", "Karim", true];];
+    
