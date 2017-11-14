daf95148fc7604078c13f3e4207295136d241f44
asad <asadkarim33@gmail.com>
2017-11-14 15:32:36 -0800

    loops practice

---
+++ loops-and-its-types/apps.js
@@ -0,0 +1,46 @@
+
+
+// var itemsArray = [
+//     name:"juice", price:"50", quantity:"3" ,
+//     name:"cookie", price:"30", quantity:"9",
+//     name:"shirt",price:"880", quantity:"1",
+//     name:"pen",price:"100", quantity:"2"
+// ];
+
+//     document.write("the total price of"+name+ "is" + price*quantity);
+    
+// loop foreach
+
+// var items = ["asad","karim","mughal"];
+
+// items.forEach(function(value,index,itemsall){
+//     document.write(value+"====" +index+"<br>");
+// })
+
+//for in 
+
+// var items = ["asad","karim","mughal"];
+
+// for (var index in items);
+
+//  document.write(index);
+
+
+//for in loop with object
+
+var items = {
+    name: "asad",
+    fatherName: "karim",
+    class: "MA"
+}
+for (var index in items)
+document.write(index+"<br>");
+
+// var words = ["spray", "limit", "elite", "exuberant", "destruction", "present", "happy"];
+// var longWords = words.filter(function(word){
+//   return word.length < 6;
+// });
+
+
+// document.write(longWords);
+//Filtered array longWords is ["exuberant", "destruction", "present"]
